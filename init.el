6602ef3a391459acb4c034766810f00f6bcb5f8f
Rob Doherty <robdoherty2@gmail.com>
2011-12-09 15:28:48 -0500

    made adjustment to clipboard paste

--- init.el
+++ init.el
@@ -58,14 +58,33 @@
 (require 'sf)
 
 ;;;;;;;;;;;;;;;;;;;;
+;; systems
+;;;;;;;;;;;;;;;;;;;;
+(defconst win32
+  (eq system-type 'windows-nt)
+  "Are we running on Win32 system?")
+(defconst linux
+  (eq system-type 'gnu/linux)
+  "Are we running on linux system?")
+(defconst graf
+  (not (eq window-system 'nil))
+  "Are we running on Windows system?")
+
+;;;;;;;;;;;;;;;;;;;;
 ;; set up unicode
-(prefer-coding-system       'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-;; This from a japanese individual.  I hope it works.
-(setq default-buffer-file-coding-system 'utf-8)
+;;(prefer-coding-system       'utf-8)
+;; This from a japanese coder.
+;;(setq default-buffer-file-coding-system 'utf-8)
 ;; From Emacs wiki
-(setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
-;; MS Windows clipboard is UTF-16LE 
-(set-clipboard-coding-system 'utf-16le-dos)
+;;(setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
+
+(when linux
+  (set-clipboard-coding-system 'koi8-u)
+  (set-default-coding-systems 'koi8-u)
+  (set-keyboard-coding-system 'koi8-u))
+(when win32
+  ;; MS Windows clipboard is UTF-16LE 
+  (set-clipboard-coding-system 'utf-16le-dos)
+  (set-default-coding-systems 'utf-8)
+  (set-terminal-coding-system 'utf-8)
+  (set-keyboard-coding-system 'utf-8))
