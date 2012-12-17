5cfab8a1d88dc7b50083fd458416a5d5c1eeea60
Robert Doherty <robert.doherty@visualrevenue.com>
2012-12-17 13:33:08 -0500

    added yaml mode

--- init.el
+++ init.el
@@ -56,6 +56,23 @@
 	  (lambda () (setq indent-tabs-mode t)
 	    (setq python-indent 8)))
 
+;; YAML mode
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;; JAVA STUFF ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; java mode use tabs
+(add-hook 'java-mode-hook
+	  (lambda () ()
+	    (setq c-basic-offset 4
+		  tab-width 4
+		  indent-tabs-mode t)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
 ;; function for fullscreen
 (defun toggle-fullscreen ()
   (interactive)
@@ -92,23 +109,10 @@
 
 (delete-selection-mode t)
 
-
 ;; fix Copy/Paste Clipboard Issues:
 (global-set-key "\C-w" 'clipboard-kill-region)
 (global-set-key "\M-w" 'clipboard-kill-ring-save)
 (global-set-key "\C-y" 'clipboard-yank)
-;;(global-set-key [(shift delete)] 'clipboard-kill-region)
-;;(global-set-key [(control insert)] 'clipboard-kill-ring-save)
-;;(global-set-key [(shift insert)] 'clipboard-yank)
-
-;;set to php-mode
-;(require 'php-mode)
-
-;; set default tab width to 8 spaces
-;;(setq-default indent-tabs-mode nil)
-;;(setq-default tab-width 8)
-;; customize c-default style variable
-;;(setq-default c-basic-offset 4)
 
 ;;;;;;;;;;;;;;;;;;;;
 ;; systems
@@ -141,49 +145,3 @@
   (set-default-coding-systems 'utf-8)
   (set-terminal-coding-system 'utf-8)
   (set-keyboard-coding-system 'utf-8))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;WANDERLUST EMAIL CLIENT;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; wanderlust
-(autoload 'wl "wl" "Wanderlust" t)
-(autoload 'wl-other-frame "wl" "Wanderlust on new frame." t)
-(autoload 'wl-draft "wl-draft" "Write draft with Wanderlust." t)
-
-;; IMAP
-(setq elmo-imap4-default-server "imap.gmail.com")
-(setq elmo-imap4-default-user "robdoherty2@gmail.com") 
-(setq elmo-imap4-default-authenticate-type 'clear) 
-(setq elmo-imap4-default-port '993)
-(setq elmo-imap4-default-stream-type 'ssl)
-
-(setq elmo-imap4-use-modified-utf7 t) 
-
-;; SMTP
-(setq wl-smtp-connection-type 'starttls)
-(setq wl-smtp-posting-port 587)
-(setq wl-smtp-authenticate-type "plain")
-(setq wl-smtp-posting-user "robdoherty2")
-(setq wl-smtp-posting-server "smtp.gmail.com")
-(setq wl-local-domain "gmail.com")
-
-(setq wl-default-folder "%inbox")
-(setq wl-default-spec "%")
-(setq wl-draft-folder "%[Gmail]/Drafts") ; Gmail IMAP
-(setq wl-trash-folder "%[Gmail]/Trash")
-
-(setq wl-folder-check-async t) 
-
-(setq elmo-imap4-use-modified-utf7 t)
-
-(autoload 'wl-user-agent-compose "wl-draft" nil t)
-(if (boundp 'mail-user-agent)
-    (setq mail-user-agent 'wl-user-agent))
-(if (fboundp 'define-mail-user-agent)
-    (define-mail-user-agent
-      'wl-user-agent
-      'wl-user-agent-compose
-      'wl-draft-send
-      'wl-draft-kill
-      'mail-send-hook))
