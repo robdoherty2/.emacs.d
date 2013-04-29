266c71f124fdb4118a4d21335f278b44c873ed4a
Robert Doherty <robert.doherty@visualrevenue.com>
2013-04-29 07:06:15 -0400

    modified init.el

--- init.el
+++ init.el
@@ -108,7 +108,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" default)))
+ '(custom-safe-themes (quote ("d6a00ef5e53adf9b6fe417d2b4404895f26210c52bb8716971be106550cea257" "71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" default)))
  '(js-indent-level 8)
  '(js2-basic-offset 8)
  '(js2-cleanup-whitespace t)
